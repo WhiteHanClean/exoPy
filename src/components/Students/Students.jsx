6d291eb6994178207af59d01c5d78b6424a158e1
Mhan <whitehanclean@gmail.com>
2023-03-06 19:26:14 +0600

    ok

--- src/components/Students/Students.jsx
+++ src/components/Students/Students.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import s from './Students.module.scss'
-
+import React, { useState, useEffect } from "react";
+import s from "./Students.module.scss";
+import axios from "axios";
 
 const Students = () => {
-    return (
-        <div className={s.students}>
-            
-        </div>
-    );
+  const [students, setstudents] = useState([]);
+  const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+  const getStudents = async () => {
+    axios.get(BASE_URL + "/users").then((res) => {
+      console.log(res);
+    });
+  };
+  useEffect(() => {
+    getStudents()
+  }, []);
+
+  return <div className={s.students}></div>;
 };
 
 export default Students;
