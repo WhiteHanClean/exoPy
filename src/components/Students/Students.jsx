4f64186969d1513c6a503a5a1191ae137784cb46
Mhan <whitehanclean@gmail.com>
2023-03-06 19:58:39 +0600

    ok

--- src/components/Students/Students.jsx
+++ src/components/Students/Students.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import s from "./Students.module.scss";
 import axios from "axios";
+import MyCard from "../Card/Card";
 
 const Students = () => {
   const [students, setStudents] = useState([]);
@@ -8,22 +9,29 @@ const Students = () => {
 
   const getStudents = async () => {
     axios.get(BASE_URL + "/users").then((res) => {
-      setStudents(res.data)
+      setStudents(res.data);
     });
   };
   useEffect(() => {
-    getStudents()
+    getStudents();
   }, []);
 
-  return <div className={s.students}>
-    {
-        students.map(student => {
-            return (
-                <h1>{student.name}</h1>
-            )
-        })
-    }
-  </div>;
+  return (
+    <div className={s.students}>
+      <div className={s.cards}>
+        {students.map((student) => {
+          return (
+            <MyCard
+              key={student.id}
+              name={student.name}
+              id={student.id}
+              img="https://s0.rbk.ru/v6_top_pics/media/img/7/76/756686017039767.jpg"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Students;
